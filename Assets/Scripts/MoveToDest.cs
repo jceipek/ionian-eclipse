fbe0372130dba22e41ed8fd8cd43c2baab3fb99e
Cypress Frankenfeld <iamcypress@gmail.com>
2013-11-15 16:42:22 -0500

    better angle finding via torque

--- Assets/Scripts/MoveToDest.cs
+++ Assets/Scripts/MoveToDest.cs
@@ -8,7 +8,7 @@ public class MoveToDest : MonoBehaviour
 	public Vector2 m_destination;
 	public Vector2 m_accel;
 	public Vector2 m_velocity;
-
+	public float m_torqueContant;
 	public Rigidbody2D m_rigidbody;
 
 
@@ -21,39 +21,31 @@ public class MoveToDest : MonoBehaviour
 	{
 		m_destination = dest;
 	}
+	
+
+	private float GetTorque(Vector3 fwd, Vector3 targetDir) {
+		var angle = Vector3.Angle(fwd, targetDir);
+		Vector3 perp = Vector3.Cross(fwd, targetDir);
+		int direction = (int)perp.normalized.z;
+		return m_torqueContant * angle * direction;
+	}
 
 	void Move ()
 	{
 //		Vector3 vel;
 		Vector3 dest = m_destination;
 //		Vector3 diff = (dest - transform.position);
-//
 //		m_accel = diff.normalized * m_accelSpeed;
 
-	
-
-
-		float k = 6;
+		float k = 60f;
 		Vector3 vec = dest - transform.position;
-//		bool adjust_angle = vec.normalized > 0.2f;
-			
-		vec = vec.normalized * k;
-		//ship.m_linearDamping = 3;
-		//ship.m_angularDamping = 60;
-		//float des_angle = Mathf.Atan2 (vec.x, -vec.y);
-		//float next_angle = (ship.GetAngle () + ship.GetAngularVelocity ()) / 3;
-		//var total_rotation = des_angle - next_angle;
-		//ship.ApplyTorque (total_rotation < 0 ? -10 : 10);
-		//if (adjust_angle) {
-		//	ship.SetAngle (des_angle);
-		m_rigidbody.AddForce (vec * 10f);
-		//	}
+		float torque = GetTorque(transform.up, vec.normalized);
+		Debug.Log (torque);
+		m_rigidbody.AddForce (vec.normalized * k);
 
-
-//		// TODO: Make this better
-//		if (diff.magnitude > 0.5f) {
-		transform.eulerAngles = new Vector3 (0, 0, Mathf.Atan2 ((dest.y - transform.position.y), (dest.x - transform.position.x)) * Mathf.Rad2Deg - 90);
-//		}
+		if (vec.magnitude > 1) {
+			m_rigidbody.AddTorque (torque);
+		}
 	}
 
 	void FixedUpdate ()
