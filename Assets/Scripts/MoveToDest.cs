265fbf1cd182a43bfe80a4b2b5fef483b11e6dc3
Brendan Ritter <brendan.ritter@yahoo.com>
2013-11-15 18:07:11 -0500

    Input from gamepad seems to be bogus.
    Horizontal seems to be -1 when vertical is 1 and pressing up, similarly
    for everything else.

--- Assets/Scripts/MoveToDest.cs
+++ Assets/Scripts/MoveToDest.cs
@@ -3,53 +3,54 @@ using System.Collections;
 
 public class MoveToDest : MonoBehaviour
 {
-	public float m_accelSpeed;
-	public float m_drag;
-	public Vector2 m_destination;
-	public Vector2 m_accel;
-	public Vector2 m_velocity;
-	public float m_torqueContant;
-	public Rigidbody2D m_rigidbody;
+		public float m_accelSpeed;
+		public float m_drag;
+		public Vector2 m_destination;
+		public Vector2 m_accel;
+		public Vector2 m_velocity;
+		public float m_torqueContant;
+		public Rigidbody2D m_rigidbody;
 
 
-	void OnEnable ()
-	{
-		m_rigidbody = GetComponent<Rigidbody2D> ();
-	}
+		void OnEnable ()
+		{
+				m_rigidbody = GetComponent<Rigidbody2D> ();
+		}
 
-	public void SetDest (Vector2 dest)
-	{
-		m_destination = dest;
-	}
+		public void SetDest (Vector2 dest)
+		{
+				m_destination = dest;
+		}
 	
 
-	private float GetTorque(Vector3 fwd, Vector3 targetDir) {
-		var angle = Vector3.Angle(fwd, targetDir);
-		Vector3 perp = Vector3.Cross(fwd, targetDir);
-		int direction = (int)perp.normalized.z;
-		return m_torqueContant * angle * direction;
-	}
+		private float GetTorque (Vector3 fwd, Vector3 targetDir)
+		{
+				var angle = Vector3.Angle (fwd, targetDir);
+				Vector3 perp = Vector3.Cross (fwd, targetDir);
+				int direction = (int)perp.normalized.z;
+				return m_torqueContant * angle * direction;
+		}
 
-	void Move ()
-	{
+		void Move ()
+		{
 //		Vector3 vel;
-		Vector3 dest = m_destination;
+				Vector3 dest = m_destination;
 //		Vector3 diff = (dest - transform.position);
 //		m_accel = diff.normalized * m_accelSpeed;
 
-		float k = 60f;
-		Vector3 vec = dest - transform.position;
-		float torque = GetTorque(transform.up, vec.normalized);
-		Debug.Log (torque);
-		m_rigidbody.AddForce (vec.normalized * k);
+				float k = 60f;
+				Vector3 vec = dest - transform.position;
+				float torque = GetTorque (transform.up, vec.normalized);
+//		Debug.Log (torque);
+				m_rigidbody.AddForce (vec.normalized * k);
 
-		if (vec.magnitude > 1) {
-			m_rigidbody.AddTorque (torque);
+				if (vec.magnitude > 1) {
+						m_rigidbody.AddTorque (torque);
+				}
 		}
-	}
 
-	void FixedUpdate ()
-	{
-		Move ();
-	}
+		void FixedUpdate ()
+		{
+				Move ();
+		}
 }
--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -5,27 +5,7 @@ using System.Collections;
 public class gameController : MonoBehaviour
 {
 		public MoveToDest m_moveToDest;
-/*
-		private bool connected = false;
-		IEnumerator CheckForControllers ()
-		{
-				while (true) {
-						var controllers = Input.GetJoystickNames ();
-						if (!connected && controllers.Length > 0) {
-								connected = true;
-								Debug.Log ("Connected");
-						} else if (connected && controllers.Length == 0) {
-								connected = false;
-								Debug.Log ("Disconnected");
-						}
-						yield return new WaitForSeconds (1f);
-				}
-		}
-		void Awake ()
-		{
-				StartCoroutine (CheckForControllers ());
-		}
-	*/
+
 		// Use this for initialization
 		void OnEnable ()
 		{
@@ -35,10 +15,16 @@ public class gameController : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-				float horiz = Input.GetAxis ("Horizontal");
-				float vert = Input.GetAxis ("Vertical");
-				Vector2 newPos = new Vector2 (transform.position.x + horiz, transform.position.y + vert);
-				Debug.DrawLine (transform.position, (new Vector3 (newPos.x, newPos.y, 0)));
-				m_moveToDest.SetDest (newPos);
+				float horizontal = Input.GetAxis ("Horizontal");
+				float vertical = Input.GetAxis ("Vertical");
+				
+/*				Vector3 horizontalNew = new Vector3 (0, 0, 0);
+				Vector3 verticalNew = new Vector3 (0, 0, 0);*/
+				
+				Debug.Log ("horizontal: " + horizontal + "vertical: " + vertical);
+/*				horizontalNew = transform.right * horizontal;
+				verticalNew = transform.up * vertical;*/
+				Vector3 newPos = new Vector3 (horizontal, vertical, 0);
+				Debug.DrawLine (new Vector3 (0, 0, 0), newPos);
 		}
 }
