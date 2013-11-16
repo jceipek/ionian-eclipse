47ac3f64eca95336ee87b1a976aa0868d548ecd9
Julian Ceipek <julian.ceipek@gmail.com>
2013-11-15 19:08:03 -0500

    Implemented controller movement

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -4,27 +4,23 @@ using System.Collections;
 [RequireComponent (typeof(MoveToDest))]
 public class gameController : MonoBehaviour
 {
-		public MoveToDest m_moveToDest;
+	public MoveToDest m_moveToDest;
 
-		// Use this for initialization
-		void OnEnable ()
-		{
-				m_moveToDest = GetComponent<MoveToDest> ();
-		}
+	public Vector3 pos;
+
+	// Use this for initialization
+	void OnEnable ()
+	{
+		m_moveToDest = GetComponent<MoveToDest> ();
+	}
 	
-		// Update is called once per frame
-		void Update ()
-		{
-				float horizontal = Input.GetAxis ("Horizontal");
-				float vertical = Input.GetAxis ("Vertical");
-				
-/*				Vector3 horizontalNew = new Vector3 (0, 0, 0);
-				Vector3 verticalNew = new Vector3 (0, 0, 0);*/
-				
-				Debug.Log ("horizontal: " + horizontal + "vertical: " + vertical);
-/*				horizontalNew = transform.right * horizontal;
-				verticalNew = transform.up * vertical;*/
-				Vector3 newPos = new Vector3 (horizontal, vertical, 0);
-				Debug.DrawLine (new Vector3 (0, 0, 0), newPos);
-		}
+	// Update is called once per frame
+	void Update ()
+	{
+		float horizontal = Input.GetAxis ("L_XAxis_1");
+		float vertical = Input.GetAxis ("L_YAxis_1");
+
+
+		m_moveToDest.SetDest (transform.position + new Vector3 (horizontal, -vertical, 0));
+	}
 }
