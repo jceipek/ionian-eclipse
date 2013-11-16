a92ceedb68f7b8997970724ee8edc216ef0ba6b4
Cypress Frankenfeld <iamcypress@gmail.com>
2013-11-16 17:20:55 -0500

    created seperate component for health and the ability to heal

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+	public float m_initialHealth = 100f;
+	private float m_health;
+	private GameObject m_shardPrefab;
+
+	void OnEnable ()
+	{
+		m_shardPrefab = Resources.Load ("Shard") as GameObject;
+	}
+
+	void Update ()
+	{
+		if (m_health <= 0) {
+			StartCoroutine (Die ());
+		}
+	}
+
+	public float ChangeHealthBy (float delta)
+	{
+		m_health += delta;
+		return m_health;
+	}
+
+	IEnumerator Die ()
+	{
+		yield return new WaitForSeconds (0.5f);
+		for (var i = 0; i < 10; i++) {
+			Vector3 direction = Random.insideUnitCircle;
+			GameObject shard = Instantiate (m_shardPrefab, transform.position + direction, Quaternion.identity) as GameObject;
+			FlyAway flyAway = shard.GetComponent<FlyAway> ();
+//			flyAway.InitWithDirectionAndTime (direction, 10f);
+		}
+		Destroy (gameObject);
+	}
+
+	public float GetHealthRatio ()
+	{
+		return m_health / m_initialHealth;
+	}
+
+
+	
+}
