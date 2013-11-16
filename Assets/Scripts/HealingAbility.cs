b5af242bb28f34b86f0aeea741541975031198b9
Cypress Frankenfeld <iamcypress@gmail.com>
2013-11-16 17:21:57 -0500

    created components for the ability to slow down ships, the ability to heal ships, and a pointer controller for the mothership

---
+++ Assets/Scripts/HealingAbility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealingAbility : MonoBehaviour
+{
+
+	public float m_healAmount = 10f;
+
+	public void Heal (Collider2D[] healedShips, int numShips)
+	{
+		for (var i = 0; i < numShips; i++) {
+			Collider2D healedShip = healedShips [i];
+			CanHeal canHeal = healedShip.GetComponent<CanHeal> ();
+			if (canHeal) {
+				canHeal.Heal (m_healAmount);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(HealingAbility))]
+[RequireComponent (typeof(SlowdownAbility))]
+
+public class PointerController : MonoBehaviour
+{
+	private HealingAbility m_healingAbility;
+	private SlowdownAbility m_slowdownAbility;
+
+	public float abilityRadius = 3f;
+	public string m_healingButton;
+	public string m_slowdownButton;
+
+	void OnEnable ()
+	{
+		m_healingAbility = GetComponent<HealingAbility> ();
+		m_slowdownAbility = GetComponent<SlowdownAbility> ();
+	}
+	
+	void Update ()
+	{
+
+		// Heal friends within a radius of your mouse, if you click the heal button
+		if (Input.GetButtonDown (m_healingButton)) {
+			Collider2D[] friends = GetFriendsNearLocation ((Vector2)Input.mousePosition);
+			m_healingAbility.Heal (friends);
+		}
+
+		// Slow down ships within a radius of your mouse, if you click the slowdown button
+		if (Input.GetButtonDown (m_slowdownButton)) {
+			Collider2D[] ships = GetEverythingNearLocation ((Vector2)Input.mousePosition);
+			m_slowdownAbility.Slowdown (ships);
+		}
+	}
+
+	Collider2D[] GetFriendsNearLocation (Vector2 location)
+	{
+		return GetEverythingNearLocation (location);
+	}
+
+	Collider2D[] GetEnemiesNearLocation (Vector2 location)
+	{
+		return GetEverythingNearLocation (location);
+	}
+
+	Collider2D[] GetEverythingNearLocation (Vector2 location)
+	{
+		return Physics2D.OverlapCircleAll (location, abilityRadius);
+	}
+	
+}
