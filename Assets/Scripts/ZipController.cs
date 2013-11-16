91043caef72acb010911d033a5ddec9ebd70b7e9
Brendan Ritter <brendan.ritter@yahoo.com>
2013-11-16 00:56:27 -0500

    Added a player that is controlled by keyboard input.

---
+++ Assets/Scripts/ZipController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZipController : MonoBehaviour
+{
+		//private MoveToDest m_moveToDest;
+		public float m_moveSpeed;
+		private Rigidbody2D m_rigidbody;
+		// Use this for initialization
+		void OnEnable ()
+		{
+				m_rigidbody = GetComponent<Rigidbody2D> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				float horizontal = Input.GetAxis ("KeyHorizontal");
+				float vertical = Input.GetAxis ("KeyVertical");
+				m_rigidbody.AddForce ((new Vector3 (horizontal, vertical, 0)) * m_moveSpeed);
+				//m_moveToDest.SetDest (transform.position + move);
+		}
+}
