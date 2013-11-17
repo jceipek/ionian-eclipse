082934528439a03327f838f15a0f1ec52b8be663
Cypress Frankenfeld <iamcypress@gmail.com>
2013-11-16 20:40:43 -0500

    added healray

---
+++ Assets/Scripts/HealRay.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealRay : MonoBehaviour
+{
+	private LineRenderer m_lineRenderer;
+	private Transform m_start;
+	private Transform m_end;
+
+	// Use this for initialization
+	void OnEnable ()
+	{
+		m_lineRenderer = GetComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Start ()
+	{
+		StartCoroutine (Die (0.3f));
+	}
+
+	void Update ()
+	{
+		if (m_start != null && m_end != null) {
+			m_lineRenderer.SetPosition (0, m_start.position);
+			m_lineRenderer.SetPosition (1, m_end.position);
+		}
+
+	}
+
+	public void SetPosition (Transform start, Transform end)
+	{
+		m_start = start;
+		m_end = end;
+	}
+
+	IEnumerator Die (float seconds)
+	{
+		yield return new WaitForSeconds (seconds);
+		Destroy (gameObject);
+	}
+}
--- Assets/Scripts/HealingAbility.cs
+++ Assets/Scripts/HealingAbility.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class HealingAbility : MonoBehaviour
 {
-
+	private GameObject m_healRayPrefab;
 	public float m_healAmount = 10f;
+
+	void OnEnable ()
+	{
+		m_healRayPrefab = Resources.Load ("HealRay") as GameObject;
+	}
+
 
 	public void Heal (Collider2D[] healedShips)
 	{
@@ -12,6 +18,9 @@ public class HealingAbility : MonoBehaviour
 			CanHeal canHeal = ship.GetComponent<CanHeal> ();
 			if (canHeal) {
 				canHeal.Heal (m_healAmount);
+				GameObject healRay = Instantiate (m_healRayPrefab) as GameObject;
+				HealRay healRayComponent = healRay.GetComponent<HealRay> ();
+				healRayComponent.SetPosition (transform, ship.transform);
 			}
 		}
 	}
