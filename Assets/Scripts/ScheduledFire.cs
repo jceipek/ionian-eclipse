a4ecd175719d5bd1c0eeadd49c2d252649207297
Chloe Eghtebas <nceghtebas@hotmail.com>
2013-11-16 22:29:44 -0500

    Enemies dont fire at each other

--- Assets/Scripts/ScheduledFire.cs
+++ Assets/Scripts/ScheduledFire.cs
@@ -4,24 +4,38 @@ using System.Collections;
 [RequireComponent (typeof(FireAbility))]
 public class ScheduledFire : MonoBehaviour
 {
-	public float m_fireFrequencySeconds;
-	private FireAbility m_fireAbility;
-	void OnEnable ()
-	{
-		m_fireAbility = GetComponent<FireAbility> ();
-	}
+		public float m_fireFrequencySeconds;
+		private FireAbility m_fireAbility;
+		private RaycastHit2D[] m_linecastResults = new RaycastHit2D[1]; // For efficient caching
 
-	// Use this for initialization
-	void Start ()
-	{
-		StartCoroutine (Fire ());
-	}
-
-	IEnumerator Fire ()
-	{
-		while (true) {
-			yield return new WaitForSeconds (m_fireFrequencySeconds);
-			m_fireAbility.Fire ();
+		void OnEnable ()
+		{
+				m_fireAbility = GetComponent<FireAbility> ();
 		}
-	}
+
+		// Use this for initialization
+		void Start ()
+		{
+				StartCoroutine (Fire ());
+		}
+
+		IEnumerator Fire ()
+		{
+				bool enemyInTheWay = false;
+				int hit;
+				
+				while (true) {	
+						yield return new WaitForSeconds (m_fireFrequencySeconds);
+						Debug.DrawLine (transform.position + transform.up, transform.position + transform.up * 10f, Color.red, 0.1f);
+						hit = Physics2D.LinecastNonAlloc (transform.position + transform.up, transform.position + transform.up * 10f, m_linecastResults);
+						if (hit > 0) {
+								enemyInTheWay = m_linecastResults [0].collider.CompareTag ("Enemy");							
+								if (!enemyInTheWay) {
+										m_fireAbility.Fire ();
+								}
+						} else {
+								m_fireAbility.Fire ();
+						}
+				}
+		}
 }
