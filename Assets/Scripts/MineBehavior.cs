5460229d2ea96b2252a0b4f34ce13de8bac87019
Cypress Frankenfeld <iamcypress@gmail.com>
2013-11-17 00:21:06 -0500

    added public mineradius variable

--- Assets/Scripts/MineBehavior.cs
+++ Assets/Scripts/MineBehavior.cs
@@ -3,53 +3,61 @@ using System.Collections;
 
 public class MineBehavior : MonoBehaviour
 {
-		public float m_force = 1000f;	
-		public float m_damage = 30f;
-		public GameObject m_visualRadius;
-		private Collider2D[] m_overlapCircleResults = new Collider2D[20];
-		void Start ()
-		{
-				StartCoroutine (Pulse (2f));
+	public float m_force = 1000f;	
+	public float m_damage = 30f;
+	public float m_radius = 6f;
+	public GameObject m_visualRadius;
+	private Collider2D[] m_overlapCircleResults = new Collider2D[20];
+	private float m_spriteWidth;
+
+	void OnEnable ()
+	{
+		m_spriteWidth = m_visualRadius.GetComponent<SpriteRenderer> ().sprite.bounds.extents.x;
+	}
+
+	void Start ()
+	{
+		StartCoroutine (Pulse (2f));
+	}
+
+	IEnumerator Pulse (float seconds)
+	{
+		float total = seconds;
+		float delta = 0.01f;
+		while (total > 0) {
+			yield return new WaitForSeconds (delta);
+			total -= delta;
+			m_visualRadius.transform.localScale = Vector3.one * Mathf.Sin (1 / total * 10f);
 		}
 
-		IEnumerator Pulse (float seconds)
-		{
-				float total = seconds;
-				float delta = 0.01f;
-				while (total > 0) {
-						yield return new WaitForSeconds (delta);
-						total -= delta;
-						m_visualRadius.transform.localScale = Vector3.one * Mathf.Sin (1 / total * 10f);
+		StartCoroutine (Explode (0.1f));
+	}
+
+	IEnumerator Explode (float seconds)
+	{
+		float total = seconds;
+		float delta = 0.01f;
+		while (total > 0) {
+			yield return new WaitForSeconds (delta);
+			total -= delta;
+			m_visualRadius.transform.localScale = Vector3.Lerp (Vector3.one * m_radius / m_spriteWidth, Vector3.zero, total / seconds);
+		}
+
+		CheckHit ();
+		Destroy (gameObject);
+	}
+
+	void CheckHit ()
+	{
+		int hitCount = Physics2D.OverlapCircleNonAlloc ((Vector2)transform.position, m_radius, m_overlapCircleResults);
+		for (var i = 0; i < hitCount; i++) {
+			Collider2D unfortunate = m_overlapCircleResults [i];
+			if (unfortunate) {
+				BulletHit bulletHit = unfortunate.GetComponent<BulletHit> ();
+				if (bulletHit) {
+					bulletHit.GetHit (m_damage, (transform.position - unfortunate.transform.position) * -m_force);
 				}
-
-				StartCoroutine (Explode (0.1f));
+			}
 		}
-
-		IEnumerator Explode (float seconds)
-		{
-				float total = seconds;
-				float delta = 0.01f;
-				while (total > 0) {
-						yield return new WaitForSeconds (delta);
-						total -= delta;
-						m_visualRadius.transform.localScale = Vector3.Lerp (Vector3.one * 6f, Vector3.zero, total / seconds);
-				}
-
-				CheckHit ();
-				Destroy (gameObject);
-		}
-
-		void CheckHit ()
-		{
-				int hitCount = Physics2D.OverlapCircleNonAlloc ((Vector2)transform.position, 6f, m_overlapCircleResults);
-				for (var i = 0; i < hitCount; i++) {
-						Collider2D unfortunate = m_overlapCircleResults [i];
-						if (unfortunate) {
-								BulletHit bulletHit = unfortunate.GetComponent<BulletHit> ();
-								if (bulletHit) {
-										bulletHit.GetHit (m_damage, (transform.position - unfortunate.transform.position) * -m_force);
-								}
-						}
-				}
-		}
+	}
 }
