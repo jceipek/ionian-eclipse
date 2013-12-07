3a0edd0dd58c8106a40c4991324ed7c65fcf90a9
Chloe Eghtebas <nceghtebas@hotmail.com>
2013-12-07 15:40:52 -0500

    Began implemmenting Spawners that get spawn more difficult enimes as the game progresses.

---
+++ Assets/Scripts/Spawner_VariableDifficulty.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner_VariableDifficulty : MonoBehaviour
+{
+		public GameObject m_enemyPrefab;
+		public float m_dicfficulty_level = 0.2f;
+		//TODO: Change to be dependant on time since game has started
+		public float m_spawningRate; 
+		public int m_enemyCountPerSpawn = 1;
+
+		void OnEnable ()
+		{
+				m_enemyPrefab = Resources.Load ("Enemy") as GameObject;
+		}
+	
+		void Start ()
+		{
+				StartCoroutine (Spawn ());	
+		}
+	
+		IEnumerator Spawn ()
+		{
+				while (true) {
+						yield return (new WaitForSeconds (m_spawningRate));
+						for (int i = 0; i < m_enemyCountPerSpawn; i++) {
+								GameObject enemy = Instantiate (m_enemyPrefab, transform.position, Quaternion.identity) as GameObject;
+								ScheduledFire scheduledFire = enemy.GetComponent<ScheduledFire> ();
+								scheduledFire.m_fireFrequencySeconds *= m_dicfficulty_level;
+						}
+						m_dicfficulty_level ++;
+				}
+		}
+	
+		void OnDrawGizmos ()
+		{
+				Gizmos.color = Color.cyan;
+				Gizmos.DrawCube (transform.position, Vector3.one * 2f);
+		}
+
+
+}
