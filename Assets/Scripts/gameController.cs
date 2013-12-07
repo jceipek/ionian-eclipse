8d156ae67557f8bfda0a83aaa9a87a64a2c7a2e9
Cypress Frankenfeld <iamcypress@gmail.com>
2013-12-07 00:30:52 -0500

    changed game console player to have adjustable max speed

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class gameController : MonoBehaviour
 {
-	public float m_moveSpeed;
+	public float m_acceleration;
 	public float m_torqueForce;
+	public float m_maxSpeed;
 	private Rigidbody2D m_rigidbody;
 
 	// Use this for initialization
@@ -30,7 +31,24 @@ public class gameController : MonoBehaviour
 		float lookHorizontal = Input.GetAxis ("R_XAxis_1");
 		float lookVertical = Input.GetAxis ("R_YAxis_1");
 
-		m_rigidbody.AddForce ((new Vector3 (horizontal, -vertical, 0)) * m_moveSpeed);
+		Vector3 movementForce = (new Vector3 (horizontal, -vertical, 0)) * m_acceleration;
+		// First find out what your modifier would be if the force
+		// direction was in the direction of the current velocity
+			
+		float straightMultiplier = 1 - (m_rigidbody.velocity.magnitude / m_maxSpeed);
+		// This value will be 1 if the rigidbody is moving at 0,
+		// and 0 if the rigidbody is moving at maxSpeed.
+		
+		// Then, find out what the dot product is between the force and the velocity
+		float forceDot = Vector3.Dot (m_rigidbody.velocity, movementForce);
+		
+		// Now, smoothly interpolate between full power and modified power
+		// depending on what direction the force is going!
+		Vector3 modifiedForce = movementForce * straightMultiplier;
+		Vector3 correctForce = Vector3.Lerp (movementForce, modifiedForce, forceDot);
+
+
+		m_rigidbody.AddForce (correctForce);
 
 		m_rigidbody.AddTorque (GetTorque (transform.up, (new Vector3 (lookHorizontal, -lookVertical)).normalized));
 	}
