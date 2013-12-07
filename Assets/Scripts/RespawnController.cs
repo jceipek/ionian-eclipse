9644a4c1070e138bcf2d62d28050232d0cf3b5f8
Julian Ceipek <julian.ceipek@gmail.com>
2013-12-06 21:52:45 -0500

    Implemented cap on respawn time

--- Assets/Scripts/RespawnController.cs
+++ Assets/Scripts/RespawnController.cs
@@ -1,18 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 [RequireComponent (typeof(Health))]
 public class RespawnController : MonoBehaviour
 {
-	public float m_respawnTime;
+	public float m_startingRespawnTime;
+	public float m_maxRespawnTime;
+	private Dictionary<string, float> m_respawnTimeMap = new Dictionary<string, float> ();
 
 	public void Respawn (string shipName)
 	{
-		StartCoroutine (RespawnInTime (shipName));
+		if (m_respawnTimeMap.ContainsKey (shipName)) {
+			m_respawnTimeMap [shipName] *= 2;
+			m_respawnTimeMap [shipName] = Mathf.Min (m_respawnTimeMap [shipName], m_maxRespawnTime);
+		} else {
+			m_respawnTimeMap [shipName] = m_startingRespawnTime;
+		}
+		StartCoroutine (RespawnInTime (shipName, m_respawnTimeMap [shipName]));
 	}
-	IEnumerator RespawnInTime (string shipName)
+
+	IEnumerator RespawnInTime (string shipName, float respawnTime)
 	{
-		yield return new WaitForSeconds (m_respawnTime); 
+		yield return new WaitForSeconds (respawnTime);
 		Instantiate (Resources.Load (shipName), transform.position, Quaternion.identity);
 	}
-		
 }
