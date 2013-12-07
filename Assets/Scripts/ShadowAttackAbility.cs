4fbdd890060c5801796cc7c62df98e11209647dd
Cypress Frankenfeld <iamcypress@gmail.com>
2013-12-07 04:26:15 -0500

    show line when teleporting

--- Assets/Scripts/ShadowAttackAbility.cs
+++ Assets/Scripts/ShadowAttackAbility.cs
@@ -6,9 +6,20 @@ public class ShadowAttackAbility : MonoBehaviour
 	public Transform[] m_endPoints = new Transform[2];
 	public float m_damage = 30;
 	public float m_force = 0;
+	public float m_lineFlashDuration = 0.1f;
+	public float m_lineFlashWidth = 0.2f;
+
+
+	private LineRenderer m_lineRenderer;
+
+	void OnEnable ()
+	{
+		m_lineRenderer = GetComponent<LineRenderer> ();
+	}
 
 	public void Attack ()
 	{
+		StartCoroutine (Flash ());
 		var linecastHits = Physics2D.LinecastAll (m_endPoints [0].position, m_endPoints [1].position);
 		foreach (var hit in linecastHits) {
 			if (hit.collider) {
@@ -26,4 +37,13 @@ public class ShadowAttackAbility : MonoBehaviour
 			}
 		}
 	}
+
+	private IEnumerator Flash ()
+	{
+		m_lineRenderer.SetWidth (m_lineFlashWidth, m_lineFlashWidth);
+		yield return new WaitForSeconds (m_lineFlashDuration);
+		m_lineRenderer.SetWidth (0f, 0f);
+
+	}
+	
 }
