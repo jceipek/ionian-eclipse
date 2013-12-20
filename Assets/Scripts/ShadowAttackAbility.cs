3a7702bfe81eadcbdca3da41bdc369926429859b
Julian Ceipek <julian.ceipek@gmail.com>
2013-12-20 00:51:11 -0500

    Implemented better balance; teleport ship doesn't fail to hit things.

--- Assets/Scripts/ShadowAttackAbility.cs
+++ Assets/Scripts/ShadowAttackAbility.cs
@@ -3,46 +3,46 @@ using System.Collections;
 
 public class ShadowAttackAbility : MonoBehaviour
 {
-		public Transform[] m_endPoints = new Transform[2];
-		public float m_damage = 30;
-		public float m_lineFlashDuration = 0.1f;
-		public float m_lineFlashWidth = 0.2f;
+	public Transform[] m_endPoints = new Transform[2];
+	public float m_damage = 30;
+	public float m_lineFlashDuration = 0.1f;
+	public float m_lineFlashWidth = 0.2f;
 
 
-		private LineRenderer m_lineRenderer;
+	private LineRenderer m_lineRenderer;
 
-		void OnEnable ()
-		{
-				m_lineRenderer = GetComponent<LineRenderer> ();
-		}
+	void OnEnable ()
+	{
+		m_lineRenderer = GetComponent<LineRenderer> ();
+	}
 
-		public void Attack ()
-		{
-				StartCoroutine (Flash ());
-				var linecastHits = Physics2D.LinecastAll (m_endPoints [0].position, m_endPoints [1].position);
-				foreach (var hit in linecastHits) {
-						if (hit.collider) {
-								GameObject hitObject = hit.collider.gameObject;
+	public void Attack ()
+	{
+		StartCoroutine (Flash ());
+		var linecastHits = Physics2D.LinecastAll (m_endPoints [0].position, m_endPoints [1].position);
+		foreach (var hit in linecastHits) {
+			if (hit.collider) {
+				GameObject hitObject = hit.collider.gameObject;
 
-								// don't kill your own ship
-								if (hitObject == m_endPoints [0].gameObject || hitObject == m_endPoints [1].gameObject) {
-										return;
-								}
-
-								EnergeticallySusceptible energeticallySusceptible = hitObject.GetComponent<EnergeticallySusceptible> ();
-								if (energeticallySusceptible) {
-										energeticallySusceptible.GetHit (m_damage);
-								}
-						}
+				// don't kill your own ship
+				if (hitObject == m_endPoints [0].gameObject || hitObject == m_endPoints [1].gameObject) {
+					continue;
 				}
-		}
 
-		private IEnumerator Flash ()
-		{
-				m_lineRenderer.SetWidth (m_lineFlashWidth, m_lineFlashWidth);
-				yield return new WaitForSeconds (m_lineFlashDuration);
-				m_lineRenderer.SetWidth (0f, 0f);
-
+				EnergeticallySusceptible energeticallySusceptible = hitObject.GetComponent<EnergeticallySusceptible> ();
+				if (energeticallySusceptible) {
+					energeticallySusceptible.GetHit (m_damage);
+				}
+			}
 		}
+	}
+
+	private IEnumerator Flash ()
+	{
+		m_lineRenderer.SetWidth (m_lineFlashWidth, m_lineFlashWidth);
+		yield return new WaitForSeconds (m_lineFlashDuration);
+		m_lineRenderer.SetWidth (0f, 0f);
+
+	}
 	
 }
